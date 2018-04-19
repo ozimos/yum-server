8b4a3c04be7d6bfaf3850ec99525c43027da4780
Tovieye Ozi <tovieye.ozi@gmail.com>
2018-04-19 03:00:33 +0100

    add second modal

--- UI/js/modal.js
+++ UI/js/modal.js
@@ -19,8 +19,8 @@ span.onclick = function () {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
+window.addEventListener('click', function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+});
