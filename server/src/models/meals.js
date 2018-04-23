8d0bb387834902299d1353ff04c4e9dd2b0c4e92
Tovieye Ozi <tovieye.ozi@gmail.com>
2018-04-23 18:42:24 +0100

    :construction: Feat - Add meals model
    [Starts ##156992007]

---
+++ server/src/models/meals.js
@@ -0,0 +1,16 @@
+const meals = [
+  {
+    id: 1,
+    title: 'Beef with Rice',
+    description: 'plain rice with ground beef',
+    price: 1500,
+  },
+  {
+    id: 2,
+    title: 'Beef with Fries',
+    description: 'beef slab with fried potato slivers',
+    price: 2000,
+  },
+];
+
+export default meals;
