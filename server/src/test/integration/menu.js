b4fb2667e8c6f245d1554d9ad8b7653148027698
Tovieye Ozi <tovieye.ozi@gmail.com>
2018-04-25 18:32:10 +0100

    :white_check_mark: Tests - Add integration test
    Add integration test for menu API endpoint set
    [Features #157077942]

---
+++ server/src/test/integration/menu.js
@@ -0,0 +1,43 @@
+import { expect, request, menuUrl, templateTest } from './helper';
+
+// Get  Menu
+describe('GET /menu', () => {
+  it('should return the menu for today', () =>
+    request.get(menuUrl).then((res) => {
+      expect(res.body.date.toString().substring(0, 10))
+        .to.equal(new Date().toISOString().substring(0, 10));
+    }));
+  templateTest('Get Menu', 'get', menuUrl, null, 'meals', 'object');
+});
+
+// Post Menu
+describe('POST /menu', () => {
+  const newMenu = {
+    date: '2018-05-25',
+    description: 'Continental, Local',
+    meals: [
+      {
+        id: 1,
+        title: 'Beef with Rice',
+        description: 'plain rice with ground beef',
+        price: 1500
+      },
+      {
+        id: 2,
+        title: 'Beef with Fries',
+        description: 'beef slab with fried potato slivers',
+        price: 2000
+      }
+    ]
+  };
+  it('should create a menu', () =>
+    request
+      .post(menuUrl)
+      .send(newMenu)
+      .then((res) => {
+        expect(res.body.date.toString().substring(0, 10))
+          .to.equal(newMenu.date.toString().substring(0, 10));
+        expect(res.body.description).to.equal(newMenu.description);
+      }));
+  templateTest('Add Menu', 'post', menuUrl, newMenu, 'meals', 'object', '201');
+});
