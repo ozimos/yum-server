5c84d52feca0d60a0f6f136ce2a07513ef4ae8d3
Tovieye Ozi <tovieye.ozi@gmail.com>
2018-05-01 23:51:46 +0100

    Feat/157216869/API-Postgres Add  User controller
    [Features #157216869]

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,32 +1,14 @@
 import bcrypt from 'bcryptjs';
-
+import jwt from 'jsonwebtoken';
 import Controller from './controller';
 
-export default class UserController extends Controller {
-  /**
-   *
-   *
-   * @param {any} req
-   * @returns {obj} HTTP Response
-   * @memberof UserController
-   */
-  login(req) {
-    // get user details from db
-    const record = this.model.find(elem => elem.email === req.body.email);
-    if (!record) {
-      return super.errorResponse('Account does not exist! Visit /api/v1/users/signup and register.');
-    }
-    // check if password is correct
-    const isCorrectPassword = bcrypt.compareSync(
-      req.body.password,
-      record.passwordHash
-    );
-
-    if (isCorrectPassword) {
-      return super.defaultResponse(record);
-    }
-    return super.errorResponse('Incorrect password', 406);
-  }
+/**
+ *
+ *
+ * @class UserController
+ */
+class UserController extends Controller {
+ 
 
   /**
    *
@@ -37,20 +19,56 @@ export default class UserController extends Controller {
    */
   signUp(req) {
     // check if email is available
-    const record = this.model.find(elem => elem.email === req.body.email);
+    return this.Model.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then((response) => {
+      if (response) {
+        const duplicate = response.userName === (req.body.userName || req.body.email) ? 'userName' : 'email';
+        return UserController.errorResponse(`${duplicate} has been used`, 406);
+      }
+      // create hash of password
+      const salt = bcrypt.genSaltSync(10);
+      req.body.passwordHash = bcrypt.hashSync(req.body.password, salt);
+      // remove plaintext password from record to write to db
+      delete req.body.password;
+      // create user in db
+      return this.Model.create(req.body)
+        .then(data => UserController
+          .sendResponseWithToken(data, 'Signup Successful '))
+        .catch(error => UserController.errorResponse(error.message));
+    }).catch(error => UserController.errorResponse(error.message));
+  }
 
-    if (record) {
-      return UserController.errorResponse(
-        `${record.email} has been used`,
-        406
-      );
+
+  /**
+   *
+   *
+   * @param {Sequelize<Model<Instance>>} data
+   * @param {String} extraMessage
+   * @returns {obj} HTTP Response
+   * @memberof UserController
+   */
+  static sendResponseWithToken(data, extraMessage = '') {
+    // remove password info
+    delete data.passwordHash;
+
+    let message = extraMessage;
+    const payload = {
+      isCaterer: data.isCaterer,
+      id: data.id
+    };
+    const token = jwt.sign(payload, process.env.TOKEN_PASSWORD, {
+      expiresIn: '1h'
+    });
+    if (token) {
+      message = `${message}Login Successful`;
+      return UserController.defaultResponse(data, 200, message, token);
     }
-    // create hash of password
-    const salt = bcrypt.genSaltSync(10);
-    req.body.passwordHash = bcrypt.hashSync(req.body.password, salt);
-    // remove plaintext password from record to write to db
-    delete req.body.password;
-    // create user in db
-    return super.postRecord(req);
+    message = `${message} but No token found`;
+    return UserController.errorResponse(message, 406);
   }
 }
+
+export default UserController;
