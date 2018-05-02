49c4e7d0fd386c2b9a4f6b97579b5da9d2718b2f
Tovieye Ozi <tovieye.ozi@gmail.com>
2018-05-02 09:52:20 +0100

    Feat/157216869/API-Postgres Configure helper for Postgres
    Add bcrypt, jwt, dotenv and other changes
    [Features #157216869]

--- server/test/integration/helper.js
+++ server/test/integration/helper.js
@@ -1,15 +1,43 @@
 /* eslint import/no-extraneous-dependencies: off */
 import chai from 'chai';
+import bcrypt from 'bcryptjs';
 import chaiHttp from 'chai-http';
+import dotenv from 'dotenv';
+import token from 'jsonwebtoken';
 import app from '../../src/app';
+import db from '../../src/models';
 
 chai.use(chaiHttp);
 export const {
   expect, request
 } = chai;
-// export const request = chai.request(app).keepOpen();
+dotenv.config();
+export const defaultPassword = 'test123';
+const salt = bcrypt.genSaltSync(10);
+const passwordHash = bcrypt.hashSync(defaultPassword, salt);
+
+export const {
+  User
+} = db;
+
+export const jwt = token;
+
+export const defaultUser = {
+  id: 'db5e4fa9-d4df-4352-a2e4-bc57f6b68e9b',
+  firstName: 'Tovieye',
+  lastName: 'Ozi',
+  email: 'ad.min@gmail.com',
+  passwordHash,
+  isCaterer: true
+};
+
+export const payload = {
+  isCaterer: defaultUser.isCaterer,
+  id: defaultUser.id
+};
+
 // endpoint urls
-const rootURL = '/api/v1';
+export const rootURL = '/api/v1';
 export const mealsUrl = `${rootURL}/meals`;
 export const mealIdUrl = `${rootURL}/meals/1`;
 export const menuUrl = `${rootURL}/menu`;
@@ -23,14 +51,14 @@ export const orderIdUrl = `${rootURL}/orders/1`;
  * @param {string} title name of test
  * @param {string} method HTTP verb
  * @param {string} url API Endpoint
- * @param {object} payload req.body content
+ * @param {object} content req.body content
  * @param {string} key one key in res.body
  * @param {string} type data type in res.body
  * @param {string} status HTTP response status
  * @returns {function} mocha test suite
  */
 
-export const templateTest = function generateTest(title, method, url, payload, key, type, status = '200') {
+export const templateTest = function generateTest(title, method, url, content, key, type, status = '200') {
   let requester, boundRequest;
   beforeEach('create http server', () => {
     requester = request(app);
@@ -40,7 +68,7 @@ export const templateTest = function generateTest(title, method, url, payload, k
   describe(title, () => {
     it('return 200 for successful', async () => {
       try {
-        const res = await boundRequest().send(payload);
+        const res = await boundRequest().send(content);
         return expect(res).to.have.status(status);
       } catch (err) {
         throw err;
@@ -49,7 +77,7 @@ export const templateTest = function generateTest(title, method, url, payload, k
 
     it('response should be json', async () => {
       try {
-        const res = await boundRequest().send(payload);
+        const res = await boundRequest().send(content);
         return expect(res).to.have.header('content-type', /json/);
       } catch (err) {
         throw err;
@@ -57,7 +85,7 @@ export const templateTest = function generateTest(title, method, url, payload, k
     });
     it('response should have required keys', async () => {
       try {
-        const res = await boundRequest().send(payload);
+        const res = await boundRequest().send(content);
         return expect(res.body).to.include.all.keys(key);
       } catch (err) {
         throw err;
@@ -65,7 +93,7 @@ export const templateTest = function generateTest(title, method, url, payload, k
     });
     it('response message to be of required type', async () => {
       try {
-        const res = await boundRequest().send(payload);
+        const res = await boundRequest().send(content);
         return expect(res.body).to.be.an(type);
       } catch (err) {
         throw err;
