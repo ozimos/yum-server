6c04c0a4944a1efe0895c52156019763eaee608d
Tovieye Ozi <tovieye.ozi@gmail.com>
2018-05-02 09:38:19 +0100

    Feat/157216869/API-Postgres Add unit test for base controller
    [Features #157216869]

--- server/test/unit/controllers/controller.js
+++ server/test/unit/controllers/controller.js
@@ -1,157 +1,198 @@
 /* eslint import/no-extraneous-dependencies: off */
+
 import {
   expect
 } from 'chai';
-import Controller from '../../../src/controllers/controller';
+import td from 'testdouble';
+import Controller from '../../../src/controllers/controller.js';
 
-describe('Controller', () => {
+const Table = td.object();
+const controller = new Controller(Table);
+describe('Center Controllers', () => {
+  afterEach(() => td.reset());
+  describe('getAllRows()', () => {
+    it('should return a list of rows if data is returned from database', () => {
+      const expectedResponse = [
+        {
+          id: 1,
+          title: 'Beef with Rice',
+          description: 'plain rice with ground beef',
+          price: 1500,
+        },
+        {
+          id: 2,
+          title: 'Beef with Fries',
+          description: 'beef slab with fried potato slivers',
+          price: 2000,
+        }
+      ];
 
-  const model = [
-    {
-      id: 1,
-      title: 'Beef with Rice',
-      description: 'plain rice with ground beef',
-      price: 1500,
-    },
-    {
-      id: 2,
-      title: 'Beef with Fries',
-      description: 'beef slab with fried potato slivers',
-      price: 2000,
-    },
-  ];
-  const noModel = undefined;
-  const errorResponse = {
-    message: 'records unavailable',
-    statusCode: 404
-  };
+      td.when(Table.findAll()).thenResolve(expectedResponse);
 
-  describe('getAllRecords()', () => {
-
-    const successResponse = {
-      message: null,
-      statusCode: 200
-    };
-    it('should return all records', () => {
-
-      const controller = new Controller(model);
-      successResponse.message = model;
-      expect(controller.getAllRecords()).to.eql(successResponse);
+      controller.getAllRows()
+        .then(response => expect(response.data).to.eql(expectedResponse));
     });
-
     it('should return an error message if no data in database', () => {
+      const expectedResponse = 'no records available';
 
-      const controller = new Controller(noModel);
-      expect(controller.getAllRecords()).to.eql(errorResponse);
+      td.when(Table.findAll()).thenResolve([]);
+      controller.getAllRows()
+        .then(response => expect(response.message).to.equal(expectedResponse));
+    });
+    it('should return an error message if error occurs when accessing database', () => {
+      const error = {
+        message: 'database error'
+      };
+      td.when(Table.findAll()).thenReject(error);
+      controller.getAllRows()
+        .catch(response => expect(response.message).to.equal(error.message));
     });
   });
 
-  describe('getSingleRecord()', () => {
-
-    const successResponse = {
-      message: null,
-      statusCode: 200
-    };
-    it('should return a record if data is returned from database', () => {
-      const req = {
-        params: {
-          id: 1
-        }
-      };
-      const controller = new Controller(model);
-      successResponse.message = model[req.params.id - 1];
-
-      expect(controller.getSingleRecord(req)).to.eql(successResponse);
-    });
-    it('should return an error message if no data in database', () => {
-      const req = {
-        params: {
-          id: 4
-        }
-      };
-      const controller = new Controller(model);
-
-      expect(controller.getSingleRecord(req)).to.eql(errorResponse);
-    });
-  });
-
-  describe('postRecord', () => {
+  describe('getRowById()', () => {
     const req = {
-      body: model[1]
+      params: {
+        id: 'c848bf5c-27ab-4882-9e43-ffe178c82602'
+      }
     };
-    const successResponse = {
-      message: null,
-      statusCode: 201
-    };
-    it('should create a record', () => {
-      const controller = new Controller(model);
-      successResponse.message = req.body;
+    it('should return a row if data is returned from database', () => {
+      const expectedResponse = {
+        id: req.params.id,
+        title: 'Beef with Fries',
+        description: 'beef slab with fried potato slivers',
+        price: 2000,
+      };
 
-      expect(controller.postRecord(req)).to.eql(successResponse);
+      td.when(Table.findById(req.params.id)).thenResolve(expectedResponse);
+
+
+      controller.getRowById(req)
+        .then(response => expect(response.data).to.eql(expectedResponse));
+    });
+    it('should return an error message if no data in database', () => {
+      const expectedResponse = 'no records available';
+
+      td.when(Table.findById(req.params.id)).thenResolve(null);
+      controller.getRowById(req)
+        .then(response => expect(response.message).to.equal(expectedResponse));
     });
     it('should return an error message if error occurs when accessing database', () => {
-      const controller = new Controller(noModel);
-      expect(controller.postRecord(req)).to.eql(errorResponse);
-
+      const error = {
+        message: 'database error'
+      };
+      td.when(Table.findById(req.params.id)).thenReject(error);
+      controller.getRowById(req)
+        .catch(response => expect(response.message).to.equal(error.message));
     });
   });
 
-  describe('updateRecord()', () => {
-
-    it('should update a record', () => {
+  describe('createRow()', () => {
+    it('should create a row', () => {
       const req = {
         body: {
-          title: 'Updated meal',
-          price: 1500,
-        },
-        params: {
-          id: 2
+          title: 'Beef with Fries',
+          description: 'beef slab with fried potato slivers',
+          price: 2000,
         }
       };
-      const controller = new Controller(model);
-      const { message } = controller.updateRecord(req);
-      expect(message[1].title).to.eql(req.body.title);
-
+      const returnValue = {
+        body: {
+          id: 'c848bf5c-27ab-4882-9e43-ffe178c82602',
+          title: 'Beef with Fries',
+          description: 'beef slab with fried potato slivers',
+          price: 2000,
+        }
+      };
+      td.when(Table.create(req.body)).thenResolve(returnValue.body);
+      controller.createRow(req)
+        .then((response) => {
+          expect(response.statusCode).to.equal(201);
+          expect(response.data).to.eql(returnValue.body);
+        });
     });
     it('should return an error message if error occurs when accessing database', () => {
       const req = {
-        body: {
-          title: 'Updated meal',
-          price: 1500,
-        },
-        params: {
-          id: 4
-        }
+        body: 'wrong input',
       };
-      const controller = new Controller(model);
-      expect(controller.updateRecord(req)).to.eql(errorResponse);
-
+      const error = {
+        message: 'database error'
+      };
+      td.when(Table.create(req.body)).thenReject(error);
+      controller.createRow(req)
+        .catch(response => expect(response.message).to.equal(error.message));
     });
   });
-  describe('deleteRecord()', () => {
-    const successResponse = {
-      message: null,
-      statusCode: 200
-    };
-    it('should delete a record', () => {
-      const req = {
-        params: {
-          id: 2
-        }
-      };
-      const controller = new Controller(model);
-      successResponse.message = 'Record deleted';
 
-      expect(controller.deleteRecord(req)).to.eql(successResponse);
+  describe('updateRow()', () => {
+    const req = {
+      body: {
+        id: 'c848bf5c-27ab-4882-9e43-ffe178c82602',
+        title: 'Beef with Fries',
+        description: 'beef slab with fried potato slivers',
+        price: 2000,
+      },
+      params: {
+        id: 'c848bf5c-27ab-4882-9e43-ffe178c82602'
+      }
+    };
+    it('should update a row', () => {
+      td.when(Table.update(req.body, {
+        where: {
+          id: req.params.id
+        },
+        returning: true
+      })).thenResolve(req.body);
+
+      controller.updateRow(req)
+        .then(response =>
+          expect(response.data).to.eql(req.body));
     });
     it('should return an error message if error occurs when accessing database', () => {
+      const error = {
+        message: 'database error'
+      };
+      td.when(Table.update(req.body, {
+        where: {
+          id: req.params.id
+        },
+        returning: true
+      })).thenReject(error);
+      controller.updateRow(req)
+        .catch(response => expect(response.message).to.equal(error.message));
+    });
+  });
+  describe('deleteRow()', () => {
+    const req = {
+      params: {
+        id: 'c848bf5c-27ab-4882-9e43-ffe178c82602'
+      }
+    };
+    it('should delete a row', () => {
       const req = {
         params: {
-          id: 4
+          id: 'c848bf5c-27ab-4882-9e43-ffe178c82602'
         }
       };
-      const controller = new Controller(model);
-      expect(controller.deleteRecord(req)).to.eql(errorResponse);
+      td.when(Table.destroy({
+        where: {
+          id: req.params.id
+        },
+      })).thenResolve(1);
+      controller.deleteRow(req)
+        .then(response =>
+          expect(response.data).to.eql(1));
+    });
+    it('should return an error message if error occurs when accessing database', () => {
+      const error = {
+        message: 'database error'
+      };
+      td.when(Table.destroy({
+        where: {
+          id: req.params.id
+        },
+      })).thenReject(error);
+      controller.deleteRow(req)
+        .catch(response => expect(response.message).to.equal(error.message));
     });
   });
 });
