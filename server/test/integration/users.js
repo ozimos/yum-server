7b72f21d60ad50d753beaac46fef5da62cb3017b
Tovieye Ozi <tovieye.ozi@gmail.com>
2018-05-02 09:45:53 +0100

    Feat/157216869/API-Postgr Add integration test for /auth/signup
    [Features #157216869]

---
+++ server/test/integration/users.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-console */
+import {
+  User,
+  expect,
+  request,
+  defaultUser,
+  defaultPassword,
+  rootURL
+} from './helper';
+import app from '../../src/app';
+
+describe('Routes Users', () => {
+  const signUpUrl = `${rootURL}/auth/signup`;
+  const logInUrl = `${rootURL}/auth/login`;
+
+
+  before(async () => {
+    try {
+      await User.truncate({
+        cascade: true
+      });
+      await User.create(defaultUser);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  // SignUp A User
+  describe('POST /auth/signup', () => {
+    const newUser = {
+      firstName: 'Tovieye',
+      lastName: 'Ozi',
+      email: 'tovieye.ozi@gmail.com',
+      password: 'abc123',
+      confirmPassword: 'abc123',
+    };
+    it('should signup new user', () => request(app).post(signUpUrl)
+      .send(newUser).then((res) => {
+        expect(res.body.data.email).to.equal(newUser.email);
+        // eslint-disable-next-line
+        expect(res.body.token).to.exist;
+      }));
+  });
+
+  // Login A User
+  describe.skip('POST /auth/login', () => {
+    const credentials = {
+      email: defaultUser.email,
+      password: defaultPassword,
+    };
+    it('should login new user', () => request(app).post(logInUrl).send(credentials).then((res) => {
+      expect(res.body.data.email).to.equal(defaultUser.email);
+      // eslint-disable-next-line
+      expect(res.body.token).to.exist;
+    }));
+  });
+});
