96f133609abe36160eabea14dfda2a7c291dc07c
Tovieye Ozi <tovieye.ozi@gmail.com>
2018-05-19 12:04:53 +0100

    Fix/157715024/isolate Add constraints to tables
    [Starts #157715024]

--- server/src/migrations/20180501141702-create-user.js
+++ server/src/migrations/20180501141702-create-user.js
@@ -1,58 +1,49 @@
 export default {
-  up: (queryInterface, Sequelize) =>
-    queryInterface.createTable(
-      'Users',
-      {
-        id: {
-          type: Sequelize.UUID,
-          primaryKey: true,
-          defaultValue: Sequelize.UUIDV4
-        },
-        email: {
-          type: Sequelize.STRING,
-          allowNull: false,
-          unique: 'email'
-        },
-        firstName: {
-          type: Sequelize.STRING,
-          allowNull: false
-        },
-        lastName: {
-          type: Sequelize.STRING,
-          allowNull: false
-        },
-
-        password: {
-          type: Sequelize.STRING,
-          allowNull: false
-        },
-        isCaterer: {
-          type: Sequelize.BOOLEAN,
-          defaultValue: false
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-          defaultValue: Sequelize.fn('NOW')
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-          defaultValue: Sequelize.fn('NOW')
-        }
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4
       },
-      // migration files do not add unique constraints in the same way as model definitions
-      // as documented at https://github.com/sequelize/cli/issues/272
-      // using the queryInterface.addConstraints method in a separate migration file works
-      // sometimes and fails inexplicably at others. uniqueKeys preferrable
-      {
-        uniqueKeys: {
-          email: {
-            fields: ['email']
-          }
-        }
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: 'email'
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      isCaterer: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
       }
-    ),
+    });
+    await queryInterface.addConstraint('Users', ['email'], {
+      type: 'unique',
+      name: 'email'
+    });
+  },
 
   down: queryInterface => queryInterface.dropTable('Users')
 };
