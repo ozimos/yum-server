6bb7acb113a0ee46f5f36d1fd2179b7d72826895
Tovieye Ozi <tovieye.ozi@gmail.com>
2018-05-20 19:47:43 +0100

    Chore/157720241/react Refactor UI javascript
    [Starts #157720241}

--- UI/js/modal.js
+++ UI/js/modal.js
@@ -1,26 +1,46 @@
 // Get the modal
 const modal = document.getElementById('myModal');
+const modal2 = document.getElementById('myModal2');
 
 // Get the button that opens the modal
-const btn = document.getElementsByClassName("modal-trigger");
+const btn = document.getElementsByClassName('modal-trigger');
+const btn2 = document.getElementsByClassName('modal-trigger2');
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName('close')[0];
+const span2 = document.getElementsByClassName('close')[1];
 
-
-for(let elem of btn){
-  elem.onclick = function () {
-        modal.style.display = "block";
+const modalOpen = (arrayLike, target) => {
+  if (arrayLike && target) {
+    [...arrayLike].forEach((elem) => {
+      elem.onclick = () => {
+        target.style.display = 'block';
       };
-}
+    });
+  }
+};
+modalOpen(btn, modal);
+modalOpen(btn2, modal2);
+
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-}
+
+const modalClose = (item, target) => {
+  if (item) {
+    item.onclick = () => {
+      target.style.display = 'none';
+    };
+  }
+};
+modalClose(span, modal);
+modalClose(span2, modal2);
+
 
 // When the user clicks anywhere outside of the modal, close it
-window.addEventListener('click', function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+  if (event.target === modal2) {
+    modal2.style.display = 'none';
   }
 });
