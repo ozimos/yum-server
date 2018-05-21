99d23c6529c76e15b9f42500f876bed815f279f2
Tovieye Ozi <tovieye.ozi@gmail.com>
2018-05-21 08:36:24 +0100

    Chore/157720241/react Setup webpack, react
    [Starts #157720241]

---
+++ client/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+import './styles/app.css';
+import './styles/appStyles.scss';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,36 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import CleanWebpackPlugin from 'clean-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 
+const devMode = process.env.NODE_ENV !== 'production';
 export default {
-  entry: './client/index.js',
+  entry: {
+    index: './client/index.jsx' },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [{
-      test: /\.css$/,
+      test: /\.s?[ac]ss$/,
+      use: [
+        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+        'css-loader', 'postcss-loader', 'sass-loader'
+      ],
+    },
+    {
+      test: /\.(png|jp(e*)g|svg)$/,
       use: [{
-        loader: 'style-loader'
-      },
-      {
-        loader: 'css-loader'
-      }
-      ]
+        loader: 'url-loader',
+        options: {
+          limit: 8000, // Convert images < 8kb to base64 strings
+          name: 'images/[hash]-[name].[ext]'
+        }
+      }]
     },
     {
       test: /\.js$/,
@@ -37,7 +50,26 @@ export default {
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      template: './client/index.html'
-    })
-  ]
+      template: './client/index.html',
+      filename: 'index.html',
+      inject: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
+    new CopyWebpackPlugin([
+      { from: 'client/images', to: 'images' }
+    ]),
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ],
+  },
 };
