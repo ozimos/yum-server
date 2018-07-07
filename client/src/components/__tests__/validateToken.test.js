7680528b8cfec17079c968538fdac5dc4ab73683
Tovieye Moses Ozi <tovieye.ozi@gmail.com>
2018-07-07 22:02:55 +0100

    Feat/158412968/dashboard : Add test for validateToken
    [Finishes #158412968]

---
+++ client/src/components/__tests__/validateToken.test.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
+import addDays from 'date-fns/add_days';
+import MockDate from 'mockdate';
+import validateToken from '../../services/validateToken';
+
+config();
+
+const tokenString = jwt.sign({ isCaterer: true }, process.env.TOKEN_PASSWORD, { expiresIn: '1h' });
+const token = `Bearer ${tokenString}`;
+const nextDay2 = addDays(new Date(), 2);
+
+describe('validate token', () => {
+
+  it('returns true if token has not expired', () => {
+    expect(validateToken({ token }).valid).toBe(true);
+  });
+  it('returns data in token', () => {
+    expect(validateToken({ token }).isCaterer).toBe(true);
+  });
+  describe('when token has expired', () => {
+    beforeEach(() => {
+      MockDate.set(nextDay2);
+    });
+    afterEach(() => {
+      MockDate.reset();
+    });
+    it('returns false', () => {
+      expect(validateToken({ token }).valid).toBe(false);
+    });
+  });
+});
