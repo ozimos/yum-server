d2a8f1aa7142b9c6f952a58e275b64beaf90da5a
Tovieye Moses Ozi <tovieye.ozi@gmail.com>
2020-04-17 00:15:02 +0100

    webhook with pull requests

--- src/routes/gitwebhook.js
+++ src/routes/gitwebhook.js
@@ -5,21 +5,26 @@ export default function gitwebhook(req, res) {
     const hmac = crypto.createHmac('sha1', process.env.SECRET);
     const sig = `sha1=${hmac.update(JSON.stringify(req.body)).digest('hex')}`;
     if (
-        req.headers['x-github-event'] === 'push' &&
+        req.headers['x-github-event'] === 'pull_request' &&
         crypto.timingSafeEqual(Buffer.from(sig), Buffer.from(req.headers['x-hub-signature']))
     ) {
         res.sendStatus(200);
-        const commands = [
-            'git fetch origin master',
-            'git reset --hard origin/master',
-            'git pull origin master --force',
-            'npm run migrate --env=production',
-            'npm install',
-            // your build commands here
-            'refresh',
-        ]; // fixes glitch ui
-        commands.forEach((cmd) => console.log(execSync(cmd).toString()));
-        console.log('updated with origin/master!');
+        const {
+            action,
+            pull_request: { merged },
+        } = req.body;
+        if (action === 'closed' && merged) {
+            const commands = [
+                'git fetch origin master',
+                'git reset --hard origin/master',
+                'git pull origin master --force',
+                'npm run migrate --env=production',
+                'npm install',
+                'refresh',
+            ];
+            commands.forEach((cmd) => console.log(execSync(cmd).toString()));
+            console.log('updated with origin/master!');
+        }
     } else {
         console.log('webhook signature incorrect!');
         return res.sendStatus(403);
